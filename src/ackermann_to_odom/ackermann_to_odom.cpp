0f4e22dd03aafb7110d5376ae96eee5e12bd9ac6
mice85 <miguelangel.munoz@ua.es>
2018-06-12 15:57:59 +0200

    Publications of /tf messages and tf dependences in package

--- src/ackermann_to_odom/ackermann_to_odom.cpp
+++ src/ackermann_to_odom/ackermann_to_odom.cpp
@@ -3,7 +3,6 @@
 #include "ackermann_msgs/AckermannDriveStamped.h"
 #include "nav_msgs/Odometry.h"
 #include <tf/transform_broadcaster.h>
-//#include <tf/tf.h>
 #include <time.h>
 
 double covariance;
@@ -23,114 +22,121 @@ void covariance_callback(const ackermann_msgs::AckermannDriveStamped& covariance
 int main (int argc, char** argv) {
 
   const float WHEELBASE_METERS = 1.05;
-	const int   ROWS             = 6;
-	const int   COLUMNS          = 6;
+  const int   ROWS             = 6;
+  const int   COLUMNS          = 6;
 
-	int i, j;
-	float orientation_z_prev = 0;
-	float pose_x_prev        = 0;
-	float pose_y_prev        = 0;
-	double t_1;
-	double t_2;
-
-
+  int i, j;
+  float orientation_z_prev = 0;
+  float pose_x_prev        = 0;
+  float pose_y_prev        = 0;
+  double t_1;
+  double t_2;
 
   ros::init (argc, argv, "ackermann_to_odom");
   ros::NodeHandle nh;
 
-	geometry_msgs::TransformStamped odom_trans;
-	static tf::TransformBroadcaster broadcaster;
+  geometry_msgs::TransformStamped odom_trans;
+  geometry_msgs::TransformStamped base_trans;
+  geometry_msgs::TransformStamped base_links;
+  tf::TransformBroadcaster broadcaster;
 
   //// PUBLISHER
   nav_msgs::Odometry odometry;
   ros::Publisher odometry_publisher = nh.advertise<nav_msgs::Odometry>("odometry", 0);
-  //ros::Publisher odometry_publisher("odometry", &odometry);
-  //nh.advertise(odometry_publisher);
+
 
   //// SUBSCRIBERS
   ros::Subscriber estimated_ackermann_subscriber;
   ros::Subscriber covariance_ackermann_subscriber;
   estimated_ackermann_subscriber = nh.subscribe ("estimated_ackermann_state", 0, ackermann_callback);
   covariance_ackermann_subscriber = nh.subscribe ("covariance_ackermann_state", 0, covariance_callback);
-  //ros::Subscriber<ackermann_msgs::AckermannDriveStamped> ackermann_subscriber("estimated_ackermann_state",
-  //                                                                            &ackermann_callback);
-  //nh.subscribe(ackermann_subscriber);
+
 
   ros::Rate loop_rate(10);
   t_2 = (double)ros::Time::now().toSec();
   while (ros::ok()){
 
-	  if (covariance == 0.0) covariance = 0.01;
+      if (covariance == 0.0) covariance = 0.01;
 
-		/////////////////////////////////////////////////
-		/////// Calculate odometry_publisher ////////////
-		t_1 = (double)ros::Time::now().toSec();
-		float delta_t = (float)(t_1 - t_2);
-		t_2 = (double)ros::Time::now().toSec();
-    float lineal_speed    = estimated_ackermann_state.drive.speed;
+      /////////////////////////////////////////////////
+      /////// Calculate odometry_publisher ////////////
+      t_1 = (double)ros::Time::now().toSec();
+      float delta_t = (float)(t_1 - t_2);
+      t_2 = (double)ros::Time::now().toSec();
+      float lineal_speed    = estimated_ackermann_state.drive.speed;
 
-		// Angle
-    double steering_radians = estimated_ackermann_state.drive.steering_angle * M_PI / 180.0;
-		float angular_speed_z   = (lineal_speed / WHEELBASE_METERS) * tan(steering_radians);
-		float orientation_z     = orientation_z_prev + angular_speed_z*delta_t;
-    if (abs(orientation_z) >= (2.0 * M_PI))
-		    orientation_z = orientation_z - (2.0 * M_PI);
-    tf::Quaternion quaternion = tf::createQuaternionFromRPY(0, 0, orientation_z);
+      // Angle
+      double steering_radians = estimated_ackermann_state.drive.steering_angle * M_PI / 180.0;
+      float angular_speed_z   = (lineal_speed / WHEELBASE_METERS) * tan(steering_radians);
+      float orientation_z     = orientation_z_prev + angular_speed_z*delta_t;
+      if (abs(orientation_z) >= (2.0 * M_PI))
+          orientation_z = orientation_z - (2.0 * M_PI);
+      tf::Quaternion quaternion = tf::createQuaternionFromRPY(0, 0, orientation_z);
 
-		// pose
-		float lineal_speed_x = lineal_speed * cos(orientation_z);
+      // pose
+      float lineal_speed_x = lineal_speed * cos(orientation_z);
 	  float lineal_speed_y = lineal_speed * sin(orientation_z);
-		float pose_x         = pose_x_prev + lineal_speed_x * delta_t;
-		float pose_y         = pose_y_prev + lineal_speed_y * delta_t;
+      float pose_x         = pose_x_prev + lineal_speed_x * delta_t;
+      float pose_y         = pose_y_prev + lineal_speed_y * delta_t;
 
-    // For next step
-		orientation_z_prev = orientation_z;
-		pose_x_prev        = pose_x;
-		pose_y_prev        = pose_y;
-		/////////////////////////////////////////////////
-		/////////////////////////////////////////////////
+      // For next step
+      orientation_z_prev = orientation_z;
+      pose_x_prev        = pose_x;
+      pose_y_prev        = pose_y;
+      /////////////////////////////////////////////////
+      /////////////////////////////////////////////////
 
-		// Header
+      // Header
 	  odometry.header.stamp          = ros::Time::now();
 	  odometry.header.frame_id       = "odom";
 	  odometry.child_frame_id        = "base_link";
 
-		// Twist
+      // Twist
 	  odometry.twist.twist.linear.x  = lineal_speed_x;
-		odometry.twist.twist.linear.y  = lineal_speed_y;
-		odometry.twist.twist.linear.z  = 0;
-		odometry.twist.twist.angular.x = 0;
-		odometry.twist.twist.angular.y = 0;
+      odometry.twist.twist.linear.y  = lineal_speed_y;
+      odometry.twist.twist.linear.z  = 0;
+      odometry.twist.twist.angular.x = 0;
+      odometry.twist.twist.angular.y = 0;
 	  odometry.twist.twist.angular.z = angular_speed_z;
-		for (i=0; i<COLUMNS; i++){
-			odometry.twist.covariance[i*ROWS+i] = covariance;
-		}
+      for (i=0; i<COLUMNS; i++){
+          odometry.twist.covariance[i*ROWS+i] = covariance;
+      }
 
-		// Pose
-		odometry.pose.pose.position.x    = pose_x;
-		odometry.pose.pose.position.y    = pose_y;
-		odometry.pose.pose.position.z    = 0;
-		odometry.pose.pose.orientation.x = quaternion[0];
-		odometry.pose.pose.orientation.y = quaternion[1];
-		odometry.pose.pose.orientation.z = quaternion[2];
-		odometry.pose.pose.orientation.w = quaternion[3];
-		for (i=0; i<COLUMNS; i++){
-			odometry.pose.covariance[i*ROWS+i] = covariance;
-		}
+      // Pose
+      odometry.pose.pose.position.x    = pose_x;
+      odometry.pose.pose.position.y    = pose_y;
+      odometry.pose.pose.position.z    = 0;
+      odometry.pose.pose.orientation.x = quaternion[0];
+      odometry.pose.pose.orientation.y = quaternion[1];
+      odometry.pose.pose.orientation.z = quaternion[2];
+      odometry.pose.pose.orientation.w = quaternion[3];
+      for (i=0; i<COLUMNS; i++){
+          odometry.pose.covariance[i*ROWS+i] = covariance;
+      }
 
-    // tf mensaje
-		odom_trans.header.frame_id         = "odom";
-    odom_trans.child_frame_id          = "base_link";
-		odom_trans.header.stamp            = ros::Time::now();
-    odom_trans.transform.translation.x = pose_x;
-    odom_trans.transform.translation.y = pose_y;
-    odom_trans.transform.translation.z = 0.0;
-    odom_trans.transform.rotation      = tf::createQuaternionMsgFromYaw(orientation_z);
+      ////////////////////////////////////////////////////////////////
+      // tf mensajes
+      odom_trans.header.frame_id         = "odom";
+      odom_trans.child_frame_id          = "base_link";
+      odom_trans.header.stamp            = ros::Time::now();
+      odom_trans.transform.translation.x = pose_x;
+      odom_trans.transform.translation.y = pose_y;
+      odom_trans.transform.translation.z = 0.0;
+      odom_trans.transform.rotation      = tf::createQuaternionMsgFromYaw(orientation_z);
 
-		// Topic publisher
+      base_trans.header.frame_id         = "base_link";
+      base_trans.child_frame_id          = "velodyne";
+      base_trans.header.stamp            = ros::Time::now();
+      base_trans.transform.translation.x = 0.55;
+      base_trans.transform.translation.y = 0.0;
+      base_trans.transform.translation.z = 0.0;
+      base_trans.transform.rotation      = tf::createQuaternionMsgFromYaw(0.0);
+      ////////////////////////////////////////////////////////////////
+
+      // Topic publisher
 	  odometry_publisher.publish(odometry);
-		//broadcaster.sendTransform(odom_trans);
-	  //odometry_publisher.publish(&odometry);
+      broadcaster.sendTransform(odom_trans);
+      broadcaster.sendTransform(base_trans);
 
 	  ros::spinOnce();
 	  loop_rate.sleep();
