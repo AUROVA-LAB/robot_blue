dd48e74f9bc486f7481d7096aae552b28aa85542
Ivan del Pino <idelpino@iri.upc.edu>
2018-07-04 13:23:38 +0200

    reactive_hokuyo node created!

---
+++ src/reactive_hokuyo/src/reactive_hokuyo_node.cpp
@@ -0,0 +1,9 @@
+#include "reactive_hokuyo_node.h"
+
+#include <sensor_msgs/PointCloud2.h>
+
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+#include <pcl/segmentation/sac_segmentation.h>
